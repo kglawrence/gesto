f5baacd8047b7b401aef69989e07e071c94f81d8
Keri Lawrence <kerilawrence1216@gmail.com>
2016-03-27 17:59:08 -0400

    bug fix - update cost with changes to db

--- app/routes.js
+++ app/routes.js
@@ -14,65 +14,65 @@ function getFoods(res) {
 
 module.exports = function (app) {
 
-// api ---------------------------------------------------------------------
-// get all todos
-app.get('/api/foods', function (req, res) {
-  // use mongoose to get all foods in the database
-  getFoods(res);
-});
-
-//display the cost of all food items
-app.get('/api/total', function (req, res) {
-
-  //use moongoose to get all foods in the database
-  Food.find(function (err, foods) {
-
-    //add cost of each item and compute sales tax
-    var cost = 0;
-    for (i = 0; i < foods.length; i++) {
-      if (foods[i].cost){
-      cost += foods[i].cost;
-      }
-    };
-    cost += (cost * .075);
-
-    res.json(cost);
-  });
-});
-
-// create food and send back all foods after creation
-app.post('/api/foods', function (req, res) {
-
-  // create a food, information comes from AJAX request from Angular
-  Food.create({
-    name: req.body.text,
-    cost: req.body.number,
-    done: false
-  }, function (err, food) {
-    if (err)
-      res.send(err);
-
-    // get and return all the foods after you create another
+  // api ---------------------------------------------------------------------
+  // get all todos
+  app.get('/api/foods', function (req, res) {
+    // use mongoose to get all foods in the database
     getFoods(res);
   });
 
-});
+  //display the cost of all food items
+  app.get('/api/total', function (req, res) {
 
-// delete a food
-app.delete('/api/foods/:food_id', function (req, res) {
-  Food.remove({
-    _id: req.params.food_id
-  }, function (err, food) {
-    if (err)
-      res.send(err);
+    //use moongoose to get all foods in the database
+    Food.find(function (err, foods) {
 
-    getFoods(res);
+      //add cost of each item and compute sales tax
+      var cost = 0;
+      for (i = 0; i < foods.length; i++) {
+        if (foods[i].cost){
+          cost += foods[i].cost;
+        }
+      };
+      cost += (cost * .075);
+
+      res.json(cost);
+    });
   });
-});
 
-// application -------------------------------------------------------------
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
+  // create food and send back all foods after creation
+  app.post('/api/foods', function (req, res) {
+
+    // create a food, information comes from AJAX request from Angular
+    Food.create({
+      name: req.body.text,
+      cost: req.body.number,
+      done: false
+    }, function (err, food) {
+      if (err)
+        res.send(err);
+
+      // get and return all the foods after you create another
+      getFoods(res);
+    });
+
+  });
+
+  // delete a food
+  app.delete('/api/foods/:food_id', function (req, res) {
+    Food.remove({
+      _id: req.params.food_id
+    }, function (err, food) {
+      if (err)
+        res.send(err);
+
+      getFoods(res);
+    });
+  });
+
+  // application -------------------------------------------------------------
+  app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+  });
 };
 
